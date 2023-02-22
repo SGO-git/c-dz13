51d10511cb4b77bd7e718bf5416aace262cb80da
Grigorii Skovorodnikov <gskovorod3@gmail.com>
2023-02-22 20:36:39 +0500

    add dz13

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+System.Console.Write("Imput array length ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = new int[N];
+Random rand = new Random();
+
+
+void FillArr(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(0, 100);
+    }
+
+}
+
+void PrintArr(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+
+FillArr(arr);
+PrintArr(arr);
